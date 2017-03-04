0fb7e851bb47df3d1ed9e519fce3f2b29fe8c793
chideat <chinaxiahaifeng@gmail.com>
2017-03-04 14:09:02 +0800

    update scripts

---
+++ scripts/article.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+
+curl http://pcc2:7020/api/v1/articles -d data="hello, world" -d user_id=257
--- scripts/user.sh
+++ scripts/user.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl http://127.0.0.1:7020/api/v1/register -d name="chideat" -d password="china123"
+curl http://pcc1:7000/api/v1/register -d name="chideat" -d password="china123"
